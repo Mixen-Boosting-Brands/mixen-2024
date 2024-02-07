9f0cc0456614c61fe5050a02e5b73af48d11e180
Luis Carlos Pando <hey@luiscarlospando.com>
2024-02-07 14:05:23 -0600

    Debugueo SwiperJS

--- src/swipers.js
+++ src/swipers.js
@@ -87,7 +87,7 @@ const swiperResultados = new Swiper('.swiper-resultados', {
   // Optional parameters
   spaceBetween: 30,
   slidesPerView: 1,
-  loop: true,
+  loop: false,
   breakpoints: {
     576: {
       slidesPerView: 1,
