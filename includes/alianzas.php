240f372c8f6ef5c88510730f505b55c377af5c2e
Luis Carlos Pando <hey@luiscarlospando.com>
2024-02-07 12:45:10 -0600

    Actualizar logos de alianzas

--- includes/alianzas.php
+++ includes/alianzas.php
@@ -44,6 +44,42 @@
                             <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/alianzas/canaco.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
+                            <div class="swiper-slide my-auto">
+                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/alianzas/chihuahua-futura.webp" alt="" class="img-fluid" loading="lazy">
+                            </div>
+                            <div class="swiper-slide my-auto">
+                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/alianzas/adobe.webp" alt="" class="img-fluid" loading="lazy">
+                            </div>
+                            <div class="swiper-slide my-auto">
+                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/alianzas/hubspot.webp" alt="" class="img-fluid" loading="lazy">
+                            </div>
+                            <div class="swiper-slide my-auto">
+                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/alianzas/oracle.webp" alt="" class="img-fluid" loading="lazy">
+                            </div>
+                            <div class="swiper-slide my-auto">
+                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/alianzas/heineken.webp" alt="" class="img-fluid" loading="lazy">
+                            </div>
+                            <div class="swiper-slide my-auto">
+                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/alianzas/inc-mty.webp" alt="" class="img-fluid" loading="lazy">
+                            </div>
+                            <div class="swiper-slide my-auto">
+                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/alianzas/tec-mty.webp" alt="" class="img-fluid" loading="lazy">
+                            </div>
+                            <div class="swiper-slide my-auto">
+                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/alianzas/startup-cuu.webp" alt="" class="img-fluid" loading="lazy">
+                            </div>
+                            <div class="swiper-slide my-auto">
+                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/alianzas/stella-orion.webp" alt="" class="img-fluid" loading="lazy">
+                            </div>
+                            <div class="swiper-slide my-auto">
+                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/alianzas/nova-orion.webp" alt="" class="img-fluid" loading="lazy">
+                            </div>
+                            <div class="swiper-slide my-auto">
+                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/alianzas/canaco.webp" alt="" class="img-fluid" loading="lazy">
+                            </div>
+                            <div class="swiper-slide my-auto">
+                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/alianzas/chihuahua-futura.webp" alt="" class="img-fluid" loading="lazy">
+                            </div>
                         </div>
                     </div>
                 </div>
